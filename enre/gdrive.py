18ca85ebdf2b00d50f3c89ac0832bf23f4dcc511
Pablo Rodriguez Zivic <pablo.rzivic@mercadolibre.com>
2024-06-05 23:03:36 -0300

    backup

--- enre/gdrive.py
+++ enre/gdrive.py
@@ -2,7 +2,7 @@ import shutil
 import tempfile
 from functools import partial
 from tqdm import tqdm
-from multiprocessing import Pool
+from multiprocessing import Pool, Process, Queue
 import os
 
 from apiclient.http import MediaFileUpload, MediaIoBaseDownload
@@ -57,11 +57,43 @@ class GDrive:
         finally:
             if os.path.exists(tmp_fname): os.unlink(tmp_fname)
 
+    def download_all_files_parallel_2(self, path, skip_existing, processes=10, print_progress=True):
+        """
+        utra fast implementation with producer process for pagination and process pool for concurent page fetch
+        :return:
+        """
+        if print_progress:
+            page_progress = tqdm(desc='Downloading', unit=' pages')
+            files_progress = tqdm(desc='Downloading', unit=' files')
+
+        producer = PagesProducer()
+        try:
+            producer.start()
+            while producer.is_alive():
+                files = producer.output_queue.get()
+                downloaded = 0
+                with Pool(processes) as p:
+                    compute_iterator = p.imap_unordered(
+                        partial(self.download_file, path=path, skip_existing=skip_existing), files
+                    )
+                    if print_progress: compute_iterator = apply_tqdm(compute_iterator, files_progress)
+                    downloaded += sum(compute_iterator)
+
+                if print_progress:
+                    page_progress.update()
+
+        finally:
+            producer.kill()
+
     def download_all_files_parallel(self, path, skip_existing, processes=10, print_progress=True):
+        """
+        fast implementation with pool process to fetch a page
+        """
         response = self.drive_service.files().list().execute()
 
         if print_progress:
-            total_progress = tqdm(desc='Downloading', unit=' pages')
+            page_progress = tqdm(desc='Downloading', unit=' pages')
+            files_progress = tqdm(desc='Downloading', unit=' files')
         files = response['files']
         page_no = 1
         total_downloaded = 0
@@ -81,14 +113,12 @@ class GDrive:
             files = response['files']
             page_no += 1
             if print_progress:
-                total_progress.set_description(f'Total downloaded: {total_downloaded}, covered', refresh=False)
-                total_progress.update()
-
-        # print('Finished Downloading')
+                page_progress.update()
+                files_progress.update(downloaded)
 
     def download_all_files_seq(self, path, skip_existing):
         """
-        old implementation of download files, unless for debugging, use `download_all_files`
+        old implementation of download files, unless for debugging or benchmarking use the other two ones
         """
         response = self.drive_service.files().list().execute()
 
@@ -105,3 +135,24 @@ class GDrive:
             page_no += 1
 
         print('Finished Downloading')
+
+
+class PagesProducer(Process):
+    def __init__(self):
+        super().__init__()
+        self.output_queue = Queue()
+
+    def run(self) -> None:
+        drive = GDrive()
+        response = drive.drive_service.files().list().execute()
+        files = response['files']
+        self.output_queue.put_nowait(files)
+        while response.get('nextPageToken'):
+            response = drive.drive_service.files().list(pageToken=response['nextPageToken']).execute()
+            self.output_queue.put_nowait(response['files'])
+
+
+def apply_tqdm(it, progress):
+    for e in it:
+        progress.update()
+        yield e
--- scripts/download_all.py
+++ scripts/download_all.py
@@ -11,7 +11,7 @@ def main():
     args = parser.parse_args()
     gdrive = GDrive()
     print(f'Downloading into {settings.DATA_PATH}')
-    gdrive.download_all_files_parallel(settings.DATA_PATH, skip_existing=True, processes=args.processes)
+    gdrive.download_all_files_parallel_2(settings.DATA_PATH, skip_existing=True, processes=args.processes)
 
 
 if __name__ == '__main__':
